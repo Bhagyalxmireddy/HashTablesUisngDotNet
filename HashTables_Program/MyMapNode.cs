48446e1969e83b030dbee97ce36a3a02aa258bcc
Bhagi <puppyreddy1997@gmail.com>
2020-12-10 10:36:26 +0530

    [Bhagyalaxmi]ADDED:Added methods to find the frequency of paragraph words

--- HashTables_Program/MyMapNode.cs
+++ HashTables_Program/MyMapNode.cs
@@ -4,76 +4,80 @@ using System.Text;
 
 namespace HashTables_Program
 {
-    public  class MyMapNode<K, V>
+    public class MyMapNode<K, V>
     {
-        private readonly int size;
-        private readonly LinkedList<KeyValuePair<K, V>>[] items;
-
+        public readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+       
         public MyMapNode(int size)
         {
             this.size = size;
-            this.items = new LinkedList<KeyValuePair<K, V>>[size];
-        }
-        protected int getArrayPosition(K key)
-        {
-            int position = key.GetHashCode() % size;
-            return Math.Abs(position);
-        }
-        public V Get(K key)
-        {
-            int position = getArrayPosition(key);
-            LinkedList<KeyValuePair<K, V>> linkedList = GetLinkedList(position);
-            foreach(KeyValuePair<K, V> item in linkedList)
-            {
-                if (item.key.Equals(key))
-                {
-                    return item.value;
-                }
-            }
-            return default(V);
+            this.items = new LinkedList<KeyValue<K, V>>[size];
         }
 
-        public void Add(K key, V value)
+        public struct KeyValue<k, v>
         {
-            int position = getArrayPosition(key);
-            LinkedList<KeyValuePair<K, V>> linkedList = GetLinkedList(position);
-            KeyValuePair<K, V> item = new KeyValuePair<K, V>() { key = key, value = value };
-            linkedList.AddLast(item);
+            public k Key { get; set; }
+            public v Value { get; set; }
         }
-        public void Remove(K key)
+       
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
         {
-            int position = getArrayPosition(key);
-            LinkedList<KeyValuePair<K, V>> linkedList = GetLinkedList(position);
-            bool itemFound = false;
-            KeyValuePair<K, V> founditem = default(KeyValuePair<K, V>);
-            foreach(KeyValuePair<K, V> item  in linkedList)
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
             {
-                if (item.key.Equals(key))
-                {
-                    itemFound = true;
-                    founditem = item;
-                }
-            }
-            if (itemFound)
-            {
-                linkedList.Remove(founditem);
-            }
-        }
-
-        protected LinkedList<KeyValuePair<K, V>> GetLinkedList(int position)
-        {
-            LinkedList<KeyValuePair<K, V>> linkedList = items[position];
-            if(linkedList == null)
-            {
-                linkedList = new LinkedList<KeyValuePair<K, V>>();
+                linkedList = new LinkedList<KeyValue<K, V>>();
                 items[position] = linkedList;
             }
             return linkedList;
         }
-    }
-    public struct KeyValuePair<K, V>
-    {
-        public K key { get; set; }
-        public V value { get; set; }
+        
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+            linkedList.AddLast(item);
+        }
+       
+        public int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+        
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
     }
 }
--- HashTables_Program/Program.cs
+++ HashTables_Program/Program.cs
@@ -9,20 +9,30 @@ namespace HashTables_Program
             Console.WriteLine("Welcomt to HashTable");
 
             MyMapNode<int, string> hash = new MyMapNode<int, string>(4);
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
+            /* hash.Add(0, "To");
+             hash.Add(1, "be");
+             hash.Add(2, "or");
+             hash.Add(3, "not");
+             hash.Add(4, "to");
+             hash.Add(5, "be");
 
-            string hash5 = hash.Get(5);
-            Console.WriteLine("5th index  value is : " + hash5);
+             string hash5 = hash.Get(5);
+             Console.WriteLine("5th index  value is : " + hash5);
 
-           // hash.Remove(3);
+            // hash.Remove(3);
 
-            string hash3 = hash.Get(3);
-            Console.WriteLine("3th index  value is : " + hash3);
+             string hash3 = hash.Get(3);
+             Console.WriteLine("3th index  value is : " + hash3);*/
+            FrequencyofPara para1 = new FrequencyofPara(); 
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] para = paragraph.Split(" ");
+            int key = 0;
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            para1.FrequencyOfWord(hash);
 
         }
     }
